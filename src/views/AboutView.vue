1828ced14cd3c9bfaedf692b806518a13ba7ae52
“iris <chidy76071919@hotmail.com>
2024-01-17 20:49:17 +0800

    git init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ params }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      params: {}
+    }
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_PATH);
+    console.log(import.meta.env.VITE_TEXT);
+    axios.get('https://randomuser.me/api/').then((res) => {
+      this.params = res.data.results[0]
+      console.log(this.params)
+    })
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
